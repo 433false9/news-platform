3b78dfa167fc0c97df546cf30c79bfe81a01934d
false9 <zone14@qq.com>
2023-02-19 18:09:31 +0800

    feat(news-api): 集成swagger2
    集成swagger2,并测试

--- news-api/src/main/java/org/zone14/api/controller/HelloControllerApi.java
+++ news-api/src/main/java/org/zone14/api/controller/HelloControllerApi.java
@@ -1,11 +1,16 @@
 package org.zone14.api.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author false9
  */
+@Api(value = "controller的标题", tags = {"xx功能的controller"})
 public interface HelloControllerApi {
-    @GetMapping("hello")
-    public Object hello();
+
+    @ApiOperation(value = "hello方法的接口", notes = "hello方法的接口", httpMethod = "GET")
+    @GetMapping("/hello")
+    Object hello();
 }

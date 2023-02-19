2c5a6810e5cd4aa06a4b7917c7943d00a81d1ea8
false9 <zone14@qq.com>
2023-02-19 15:37:13 +0800

    chore(配置文件): 日志与多环境配置文件
    日志与多环境配置文件

--- news-service-user/src/main/java/org/zone14/user/controller/HelloController.java
+++ news-service-user/src/main/java/org/zone14/user/controller/HelloController.java
@@ -1,5 +1,8 @@
 package org.zone14.user.controller;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.zone14.api.controller.HelloControllerApi;
 
@@ -9,8 +12,15 @@ import org.zone14.api.controller.HelloControllerApi;
 @RestController
 public class HelloController implements HelloControllerApi {
 
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Override
     public Object hello() {
+
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
+
         return  "hello";
     }
 

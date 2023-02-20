ffebd9730c75df0eafbdc2e546327d2f2cc0fca3
false9 <zone14@qq.com>
2023-02-20 09:48:27 +0800

    feat(common, service-user): 集成redis
    集成redis

--- news-service-user/src/main/java/org/zone14/user/controller/HelloController.java
+++ news-service-user/src/main/java/org/zone14/user/controller/HelloController.java
@@ -3,9 +3,12 @@ package org.zone14.user.controller;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zone14.api.controller.HelloControllerApi;
 import org.zone14.response.JsonResponse;
+import org.zone14.utils.RedisOperator;
 
 /**
  * @author false9
@@ -14,15 +17,25 @@ import org.zone14.response.JsonResponse;
 public class HelloController implements HelloControllerApi {
 
     final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private RedisOperator redis;
+
+
     @Override
     public JsonResponse hello() {
-
         logger.debug("debug: hello~");
         logger.info("info: hello~");
         logger.warn("warn: hello~");
         logger.error("error: hello~");
 
         return JsonResponse.ok("hello") ;
+    }
+
+    @GetMapping("/redis")
+    public JsonResponse redis() {
+       redis.set("age", "19");
+       return JsonResponse.ok(redis.get("age"));
     }
 
 }
